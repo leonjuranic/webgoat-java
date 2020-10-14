ce80756204ecb358410f0b295514a43d8d2b05f7
leonjuranic <leon@defensecode.com>
2020-10-14 18:49:24 +0200

    Update ViewProfile.java

--- src/main/java/org/owasp/webgoat/lessons/SQLInjection/ViewProfile.java
+++ src/main/java/org/owasp/webgoat/lessons/SQLInjection/ViewProfile.java
@@ -154,6 +154,7 @@ public class ViewProfile extends DefaultLessonAction
 				Statement answer_statement = WebSession.getConnection(s)
 						.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 				ResultSet answer_results = answer_statement.executeQuery(query);
+				ResultSet answer_results = answer_statement.executeQuery(query);
 				if (answer_results.next())
 				{
 					// Note: Do NOT get the password field.
